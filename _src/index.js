31ef9fe455a4a433cbbe7a7680ca9cb2c336ef4f
Sandoche Adittane <sandoche@protonmail.com>
2020-08-19 11:57:12 +0100

    :wrench: Updating settings of PH modal

--- _src/index.js
+++ _src/index.js
@@ -6,7 +6,7 @@ const tobi = new Tobi()
 
 // Remove the two following lines to remove the product hunt floating prompt
 import FloatingPrompt from 'producthunt-floating-prompt'
-FloatingPrompt({ name: 'Mobile App Landing Page', url: 'https://www.producthunt.com/posts/mobile-app-landing-page', bottom: '96px' })
+FloatingPrompt({ name: 'Mobile App Landing Page', url: 'https://www.producthunt.com/posts/mobile-app-landing-page', bottom: '96px', width: '450px' })
 
 // Remove the following lines to remove the darkmode js
 import Darkmode from 'darkmode-js'
