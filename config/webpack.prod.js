5b786f2dcc0f9483514ef066cee649f93f37dff5
Dmitriy Chernysh <dmitriy.chernysh@gmail.com>
2024-01-17 19:10:25 +0200

    Testing a new path

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -11,7 +11,7 @@ module.exports = Merge(CommonConfig, {
   output: {
     filename: '[name]-[hash].bundle.js',
     path: path.resolve('assets'),
-    publicPath: '/assets/',
+    publicPath: './assets/',
   },
   optimization: {
     minimize: true,
